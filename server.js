fecda6f9be3433a8330960afc2f405838c523c32
Nicholas Truson <nicholas.truson@gmail.com>
2019-10-04 10:24:32 -0700

    proj router cruds

--- server.js
+++ server.js
@@ -4,14 +4,25 @@ const server = express();
 //server.use(logger);
 server.use(express.json());
 
-//const projectRouter = require("./projectRouter");
-//const actionRouter = require("./actionRouter");
+const projectRouter = require("./projectRouter");
+const actionRouter = require("./actionRouter");
 
-//server.use("/projectRouter", projectRouter)
+server.use("/projectRouter", logger, projectRouter)
 //server.use("/actionRouter", actionRouter)
 
 server.get('/', (req, res) => {
     res.status(200).json({ welcome: 'It is Working!' });
   });
+
+// logger
+
+function logger(req, res, next) {
+    console.log(
+      `${req.method} to ${req.url}`
+    );
+    next();
+  }
+  
+  server.use(logger);
 
 module.exports = server;
